523986001c03a5f3410ae165990d70743f2e2d46
thdiaman <themisdiamantopoulos@hotmail.com>
2026-01-14 12:52:15 +0200

    Update tools and clean up examples

--- sdg/tools/dataplot.py
+++ sdg/tools/dataplot.py
@@ -1,34 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from examples.mixeddatadescriptor import MixedDataGenerator
+from examples.loandatadescriptor import LoanDatasetStreamGenerator
 
 """ Set here the sampling size for the plot """
 samplingsize = 100
 
 """ Set here the data generator (and import it in the code) """
-DataGenerator = MixedDataGenerator
+DataGenerator = LoanDatasetStreamGenerator
 
 """ Set here the number of instances """
 datasize = 10000
 
-""" Set here the concept drift positions """
-""" If only their count is given, then they are produced randomly """
-#conceptdrifts = [1924, 509, 4606, 4112]
-conceptdrifts = []
+""" Set here the concept drift positions - leave empty for no concept drifts """
+conceptdrifts = [1924, 509, 4606, 4112]
+#conceptdrifts = []
 
-""" Set here the data drift positions for each feature """
-""" If only their count is given, then they are produced randomly """
-#datadrifts = {'salary': [3757, 2386, 1779, 9035]}
-datadrifts = {}
+""" Set here the data drift positions for each feature - leave empty for no data drifts """
+datadrifts = {'salary': [3757, 2386, 1779, 9035]}
+#datadrifts = {}
 
 if __name__ == '__main__':
     gen = DataGenerator(seed = 42)
     print("Using data generator " + gen.dataset_name)
     print("Creating a dataset of " + str(datasize) + " instances")
     dataset = gen.get_n_instances(datasize)
-    if len(conceptdrifts) > 0 and gen.target_name in getattr(gen, '_drift_configs', {}):
+    if len(conceptdrifts) > 0 and gen.target_name in getattr(gen, 'drift_configs', {}):
         print("    with " + str(len(conceptdrifts)) + " concept drifts")
-    datadriftfeatures = [var for var in getattr(gen, '_drift_configs', {}) if var != gen.target_name and var in datadrifts]
+    datadriftfeatures = [var for var in getattr(gen, 'drift_configs', {}) if var != gen.target_name and var in datadrifts]
     for feature in datadriftfeatures:
         print("    with " + str(len(datadrifts[feature])) + " data drifts for feature " + feature)
     print("\n")
